5adf057904f362ac1372ee6e5d64954ef11a1e9b
Claudio Carigé <ccarige@gmail.com>
2024-05-11 21:51:30 -0300

    [ addition ] : Created Part class

---
+++ src/main/java/com/claudiocarige/googleiagemini/core/domain/entities/Part.java
@@ -0,0 +1,13 @@
+package com.claudiocarige.googleiagemini.core.domain.entities;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Part {
+
+    private String text;
+}
