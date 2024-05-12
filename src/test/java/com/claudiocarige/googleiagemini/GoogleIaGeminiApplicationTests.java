6a57e5fc340a01b80910b231648a09d22d810e0c
Claudio Carigé <ccarige@gmail.com>
2024-05-12 01:46:07 -0300

    [ refactor ] : Refactor GoogleIaGeminiApplicationTests class

--- src/test/java/com/claudiocarige/googleiagemini/GoogleIaGeminiApplicationTests.java
+++ src/test/java/com/claudiocarige/googleiagemini/GoogleIaGeminiApplicationTests.java
@@ -1,13 +1,11 @@
 package com.claudiocarige.googleiagemini;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
 class GoogleIaGeminiApplicationTests {
 
     @Test
     void contextLoads() {
     }
-
 }
