66451853725956ec1cfd7d0bddfdf1a153fff9b5
Claudio Carigé <ccarige@gmail.com>
2024-05-12 01:30:42 -0300

    [ addition ] : Created AppConfig configuration class

---
+++ src/main/java/com/claudiocarige/googleiagemini/infra/config/AppConfig.java
@@ -0,0 +1,33 @@
+package com.claudiocarige.googleiagemini.infra.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class AppConfig implements WebMvcConfigurer {
+
+    @Bean
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*").allowedMethods("*");
+            }
+        };
+    }
+
+    @Bean
+    public HttpHeaders httpHeaders(){
+        return new HttpHeaders();
+    }
+
+}
