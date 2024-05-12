96c951d162556cba0b8a94fc6998a1f64f865d7a
Claudio Carigé <ccarige@gmail.com>
2024-05-11 22:01:48 -0300

    [ addition ] : OpenApiConfig configuration created for documentation

---
+++ src/main/java/com/claudiocarige/googleiagemini/infra/config/OpenApiConfig.java
@@ -0,0 +1,25 @@
+package com.claudiocarige.googleiagemini.infra.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Criador de prompt para buscas com o Gemini")
+                        .version("v1")
+                        .description("Esta API irá te ajuadar a construir um prompt para ser usado em uma iteração com o Gemini IA" +
+                                "Criando de forma simples um contexto para a geração da sua necessidade textual para iteração como o " +
+                                "modelo de linguagem")
+                        .termsOfService("https://sua-api-exemplo.com/termos-de-servico")
+                        .license(new License()
+                                .name("Apache 2.0")
+                                .url("https://springdoc.org")));
+    }
+}
