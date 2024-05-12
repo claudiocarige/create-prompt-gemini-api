71291c3b62d5923baaaade3e2644a08fdd8f9f5e
Claudio Carigé <ccarige@gmail.com>
2024-05-11 21:59:00 -0300

    [ addition ] :

---
+++ src/main/java/com/claudiocarige/googleiagemini/GoogleIaGeminiApplication.java
@@ -0,0 +1,13 @@
+package com.claudiocarige.googleiagemini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleIaGeminiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoogleIaGeminiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/claudiocarige/googleiagemini/GoogleIaGeminiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.claudiocarige.googleiagemini;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoogleIaGeminiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
