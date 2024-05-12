1ca2063e65e7a16c4b377a6212ec7bc7448dc7cc
Claudio Carigé <ccarige@gmail.com>
2024-05-11 23:21:06 -0300

    [ refactor ] : Refactored the descriptions

--- src/main/java/com/claudiocarige/googleiagemini/infra/config/OpenApiConfig.java
+++ src/main/java/com/claudiocarige/googleiagemini/infra/config/OpenApiConfig.java
@@ -12,9 +12,9 @@ public class OpenApiConfig {
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
                 .info(new Info()
-                        .title("Criador de prompt para buscas com o Gemini")
+                        .title("Criador de prompt para interagir com o Google Gemini AI")
                         .version("v1")
-                        .description("Esta API irá te ajuadar a construir um prompt para ser usado em uma iteração com o Gemini IA" +
+                        .description("Esta API irá te ajuadar a construir um prompt para ser usado em uma iteração com o Gemini AI" +
                                 "Criando de forma simples um contexto para a geração da sua necessidade textual para iteração como o " +
                                 "modelo de linguagem")
                         .termsOfService("https://sua-api-exemplo.com/termos-de-servico")
--- src/main/java/com/claudiocarige/googleiagemini/infra/controllers/RootController.java
+++ src/main/java/com/claudiocarige/googleiagemini/infra/controllers/RootController.java
@@ -23,23 +23,30 @@ public class RootController {
     }
 
     @PostMapping
-    @Operation(summary = "Criar Prompt", description = "Esta **API** criará um prompt para iteração com o **Gemini IA**. \n\n" +
-            "Com o atributo ''tipo'' você poderá escolher um modelo de saida, ou seja, um texto, um artigo, uma frase, " +
+    @Operation(summary = "Criar Prompt", description = "Esta **API** criará um prompt para iteração com o " +
+            "**Google Gemini AI**. \n\n" +
+            "Com o atributo ''tipo'' você poderá escolher uma estrutura de saida, ou seja, um texto, um artigo, uma frase, " +
             "uma pergunta, um comentário...\n\n" +
-            "Com o atributo ''sobre'' você pode direcionar para o tema desejado, por exemplo falar sobre: linguagem Java, " +
-            "Gemini, qualquer tema da sua preferencia.\n\n" +
+            "Com o atributo ''sobre'' você pode direcionar para o tema desejado, por exemplo falar sobre: " +
+            "linguagem Java, Gemini, qualquer tema da sua preferencia.\n\n" +
             "Para especificar melhor, vou exemplificar:\n\n" +
             "* **''tipo''** : ''Artigo''\n\n" +
             "* **''sobre''** : ''Linguagem java''\n\n" +
             "**Sobre a configuração de Criatividade temos:**\n\n" +
             "* **''temperature''** -> controla o quão será criativo de 0.0 a 1.0\n" +
-            "* **''maxOutputTokens''** ->  Um token tem cerca de quatro caracteres. 100 tokens correspondem a aproximadamente 60 a 80 palavras\n" +
-            "* **''topK''** -> Muda a forma como o modelo seleciona tokens para saída. O valor topK de 1 indica que o token selecionado é o mais provável entre todos no vocabulário do modelo (também chamado de decodificação gananciosa), enquanto topK de 3 significa que o próximo token é selecionado entre os três mais prováveis de usar a temperatura\n" +
-            "* **''topP''** -> Os tokens são selecionados do mais ao menos provável até que a soma das probabilidades seja igual ao valor de topP\n\n" +
+            "* **''maxOutputTokens''** ->  Um token tem cerca de quatro caracteres. 100 tokens correspondem a " +
+            "aproximadamente 60 a 80 palavras\n" +
+            "* **''topK''** -> Muda a forma como o modelo seleciona tokens para saída. O valor topK de 1 indica " +
+            "que o token selecionado é o mais provável entre todos no vocabulário do modelo (também chamado de " +
+            "decodificação gananciosa), enquanto topK de 3 significa que o próximo token é selecionado entre " +
+            "os três mais prováveis de usar a temperatura\n" +
+            "* **''topP''** -> Os tokens são selecionados do mais ao menos provável até que a soma das" +
+            " probabilidades seja igual ao valor de topP\n\n" +
             "**Fique a vontade para testar a API**",
             responses = {
                     @ApiResponse(description = "Success", responseCode = "200",
-                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = GeminiRequest.class))
+                            content = @Content(mediaType = "application/json",
+                                                    schema = @Schema(implementation = GeminiRequest.class))
                     ),
                     @ApiResponse(responseCode = "204", description = "No content", content = @Content),
                     @ApiResponse(responseCode = "400", description = "Bad Request", content = @Content),
